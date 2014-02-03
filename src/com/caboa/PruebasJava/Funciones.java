04d4e05538df0b76e8edbeff1e8dbac6c92157a3
Fran Romero <f.romeromedina@gmail.com>
2014-02-03 23:36:34 +0100

    Agregada  función que descarga archivo a la raíz del sistema.

--- src/com/caboa/PruebasJava/Funciones.java
+++ src/com/caboa/PruebasJava/Funciones.java
@@ -1,6 +1,11 @@
 package com.caboa.PruebasJava;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.Scanner;
+import java.net.*;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 
 public class Funciones
 {
@@ -13,14 +18,37 @@ public class Funciones
     //Función que suma dos números.
     public void sumarEntrada()
     {
+        //Crea las variables con el resultado y las dos entradas.
         int x, y, resultado;
-        Scanner entrada = new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in); //Pide entrada.
         System.out.println("Introduce un número: ");
-        x = entrada.nextInt();
+        x = entrada.nextInt(); //Pide otra entrada.
         System.out.println("Introduce otro número; ");
-        y = entrada.nextInt();
+        y = entrada.nextInt(); //Suma la entrada.
         resultado = x + y;
-        System.out.println(x + " + " + y + " es igual a: " + resultado);
+        System.out.println(x + " + " + y + " es igual a: " + resultado); //Lo saca todo por la salida de consola.
+    }
+    
+    public void descargarArchivo()
+    {
+        try
+        {
+            File directory = new File("\\PruebaJavaDescarga");
+            if(!directory.exists())
+            {
+                directory.mkdir();
+            }
+            URL website = new URL("http://www.quickmeme.com/img/a8/a8aa5dc509f0d6fc9269c8be93e4341c83645c6120fdcd9d0f983fbb92274bf1.jpg".toString());
+            ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+            FileOutputStream fos = new FileOutputStream("\\PruebaJavaDescarga\\imagen.jpg");
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+            System.out.println("Archivo descargado.");
+        }
+        catch (Exception ex)
+        {
+            System.out.println(ex.toString());
+        }
+        
     }
     
 }
--- src/com/caboa/PruebasJava/Main.java
+++ src/com/caboa/PruebasJava/Main.java
@@ -10,6 +10,7 @@ public class Main
     public static void main (String [] arg)
     {
         funciones.holaMundo();
-        funciones.sumarEntrada();
+        //funciones.sumarEntrada();
+        funciones.descargarArchivo();
     }
 }
